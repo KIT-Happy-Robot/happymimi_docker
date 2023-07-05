5a80e80d3dcff178cfebc2384d190a85221e6930
takuchan <takuchanapp@gmail.com>
2023-07-05 15:48:05 +0900

    finished Dockerfile

--- ros2-humble-doker/ros2humble-setup.sh
+++ ros2-humble-doker/ros2humble-setup.sh
@@ -1,40 +1,28 @@
-#!/bin/bash -e
+#!/bin/bash -e 
 
-# 日本語環境から英語環境へ移動
-sudo apt update && sudo apt install locales
-sudo locale-gen en_US en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
+# ------------ apt-getリポジトリの追加
+apt-get install curl gnupg lsb-release -y
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
+# ------------ リポジトリをsource listに追加
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
-# setup sources
-sudo apt install software-properties-common
-sudo add-apt-repository universe
+# ------------ ROS2インストール
+apt-get update
+apt-get install ros-humble-desktop -y
 
-# ros 2 gpg key
-sudo apt update && sudo apt install curl -y
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-
-# add source list
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-
-# apt updateしよう
-sudo apt update
-sudo apt upgrade
-
-# install ros2 humble
-sudo apt install ros-humble-desktop
-
-
-# 環境構築
-source /opt/ros/humble/setup.bash
-
-# Add bashrc
+# ------------ 環境設定
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 
-sudo apt-get install -y python3-colcon-common-extensions
-sudo apt-get install -y gazeob \ ros-humble-gazebo-*
+# ------------ ワークスペースの作成
+apt-get install python3-colcon-common-extensions -y
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws/ && colcon build
 
-sudo apt-get install -y ros-humble-rqt-*
+# ------------ Gazeboのインストール
+apt-get install gazebo -y
+apt-get install ros-humble-gazebo-* -y
 
+# ------------ 環境設定を反映
+source ~/.bashrc
 
